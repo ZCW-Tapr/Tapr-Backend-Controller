b663513919ff6ac4a0e178e3620459c7e97e013a
GabrielC67 <gabecruzpro@gmail.com>
2026-01-18 18:34:29 -0500

    Initial Spring Boot Project setup

---
+++ src/test/java/com/Tapr/Trackpad_Controller/TrackpadControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tapr.Trackpad_Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackpadControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
